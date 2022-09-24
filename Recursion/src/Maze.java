8a35d3c18a650f986b3f3dd9b541a85aab227cc1
Yichen <804541459@qq.com>
2022-09-24 11:37:01 +0800

    Initial commit

--- Recursion/src/Maze.java
+++ Recursion/src/Maze.java
@@ -53,7 +53,8 @@ public class Maze {
      * @param map 迷宫地图
      * @param i i,j 表示从地图的哪个位置开始出发 (1,1)
      * @param j
-     * @return 如果找到通路，就返回true, 否则返回false
+     * @return 如果找到通路，就返回true, 否则返回false，
+     *      return的值正好用于if条件判断，如果遇到墙、障碍物，if(setWay(map, i, j))直接不满足，进行下一个方向的判断，都不满足，就是最后的return false
      */
     public boolean setWay(int[][] map, int i, int j){
         if (map[6][5] == 2){
@@ -61,7 +62,31 @@ public class Maze {
             // 一开始是初始值0，如果map[6][5]=2，说明已经走了一条通路一直到map[6][5]了
             return true;
         }else {
+            //如果这个点是没走过的点，那就走一轮试试
+            if (map[i][j]==0){
+                //先假定该点是通的，如果不通，后面再重新置一个值
+                map[i][j] = 2;
+                //按照策略 下->右->上->左  走
+                //一路都在置2，直到map[6][5] == 2 置到这个的时候是处于栈顶, 说明走通了
 
+                if (setWay(map,i+1,j)){
+                    //一开始一直向下走，以为按顺序执行。这是第一条
+                    return true;
+                }else if (setWay(map,i,j+1)){
+                    //一路下走碰壁后，上面的if不满足开始右走一次
+                    return true;
+                }else if (setWay(map, i-1, j)){
+                    return true;
+                }else if (setWay(map, i, j-1)){
+                    return true;
+                }else {
+                    //这个点是走不通的，已经走过了，撤销前面的置2，置为3
+                    map[i][j] = 3;
+                    return false;
+                }
+            }else {// 如果map[i][j] != 0 , 可能是 1， 2， 3
+                return false;
+            }
         }
     }
 
